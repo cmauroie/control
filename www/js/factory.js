076137541a4aa3aa10dfc1f3dd6a1f4b6fdc8fb6
mauricio <ing-mauricio@outlook.com>
2015-06-10 11:46:57 -0500

    Resource_199

--- www/js/factory.js
+++ www/js/factory.js
@@ -12,8 +12,17 @@ angular.module('factor',['ngResource'])
 	return $resource('http://199.180.186.183/rest/access/initialData',{ },{
 	//return $resource('http://192.168.34.21/Modipay/rest/access/initialData', { }, {
 		update: {
-			method:'GET',
-			headers: {'Authorization':'Basic MzcyNDI4OnZhbGxlMTIzNA=='}
+
+			method:'GET',     
+			headers:{
+        'Content-Type':'application/json','Authorization':'Basic MzcyNDI4OnZhbGxlMTIzNA=='}
  			}
  });
 });
+
+/*
+'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+      'Access-Control-Allow-Origin':'Context.Request.Headers["Origin"]',
+      'Access-Control-Allow-Methods':'GET, POST PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Credentials':'true',
+*/
